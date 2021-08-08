8df6f5953ad95b43917f1b6dfb3ee04125372f89
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-08-09 00:49:01 +0900

    feat: Success to make OpenCV Edge Detecting

--- app/src/main/java/com/hankyo/jeong/lachapelle/MainActivity.java
+++ app/src/main/java/com/hankyo/jeong/lachapelle/MainActivity.java
@@ -1,16 +1,27 @@
 package com.hankyo.jeong.lachapelle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Debug;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,14 +29,16 @@ import android.widget.Toast;
 import com.hankyo.jeong.lachapelle.databinding.ActivityMainBinding;
 
 import org.jetbrains.annotations.NotNull;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
+    static final String TAG = "Lachapelle";
     static final int PERMISSIONS_REQUEST_CODE = 1000;
     String[] PERMISSIONS = {"android.permission.READ_EXTERNAL_STORAGE"};
 
-    // JNI
-    public native void detectEdgeJNI(long inputImage, long outputImage, int th1, int th2);
+    static final int REQ_CODE_SELECT_IMAGE = 100;
 
     // Used to load the 'native-lib' library on application startup.
     static
@@ -37,6 +50,9 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
     private ImageView mImageView;
     private ImageView mEdgeImageView;
+
+    private Bitmap mInputImage;
+    private Bitmap mOriginalImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -57,14 +73,72 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         setUI();
 
         // Example of a call to a native method
-//        TextView tv = binding.sampleText;
-//        tv.setText(stringFromJNI());
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mInputImage.recycle();
+        if (mInputImage != null)
+            mInputImage = null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_CODE_SELECT_IMAGE)
+        {
+            if (resultCode == Activity.RESULT_OK)
+            {
+                String path = getImagePathFromURI(data.getData());
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 4;
+                mOriginalImage = BitmapFactory.decodeFile(path, options);
+                mInputImage = BitmapFactory.decodeFile(path, options);
+
+                if (mInputImage != null)
+                    detectEdgeUsingJNI();
+            }
+        }
     }
 
     private void setUI()
     {
         mImageView = binding.originIv;
         mEdgeImageView = binding.edgeIv;
+    }
+
+    private String getImagePathFromURI(Uri contentUri)
+    {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
+        if (cursor == null)
+            return contentUri.getPath();
+        else
+        {
+            int idx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String imgPath = cursor.getString(idx);
+            cursor.close();
+
+            return imgPath;
+        }
+    }
+
+    private void detectEdgeUsingJNI()
+    {
+        Mat src = new Mat();
+        Utils.bitmapToMat(mInputImage, src);
+        mImageView.setImageBitmap(mOriginalImage);
+
+        Mat edge = new Mat();
+        detectEdgeJNI(src.getNativeObjAddr(), edge.getNativeObjAddr(), 50, 150);
+        Utils.matToBitmap(edge, mInputImage);
+        mEdgeImageView.setImageBitmap(mInputImage);
     }
 
     private boolean hasPermissions(String[] permissions)
@@ -129,9 +203,20 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         builder.create().show();
     }
 
+    public void onButtonClicked(View view)
+    {
+        Log.d(TAG, "Image Load Start!!");
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+    }
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public native void detectEdgeJNI(long inputImage, long outputImage, int th1, int th2);
 }
