67390c02ba873e14b5a92c632d343d6d9ab206e7
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-08-08 15:56:50 +0900

    refactor: git add app/src path

---
+++ app/src/main/java/com/hankyo/jeong/lachapelle/MainActivity.java
@@ -0,0 +1,96 @@
+package com.hankyo.jeong.lachapelle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.hankyo.jeong.lachapelle.databinding.ActivityMainBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    static final int PERMISSIONS_REQUEST_CODE = 1000;
+    String[] PERMISSIONS = {"android.permission.READ_EXTERNAL_STORAGE"};
+
+    // Used to load the 'native-lib' library on application startup.
+    static
+    {
+        System.loadLibrary("native-lib");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (!hasPermissions(PERMISSIONS))
+            {
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        }
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    private boolean hasPermissions(String[] permissions)
+    {
+        int result;
+        for (String perms: permissions)
+        {
+            result = ContextCompat.checkSelfPermission(this, perms);
+            if (result == PackageManager.PERMISSION_DENIED)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults.length > 0)
+                {
+                    boolean externalStoragePermissionAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if (!externalStoragePermissionAccepted)
+                    {
+
+                    }
+                }
+                break;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg)
+    {
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
