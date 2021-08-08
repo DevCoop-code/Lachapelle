652bb663dfb573bea48ecd3b4eb874624e5ac297
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-08-08 23:54:32 +0900

    feat: Setting CMakeLists

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/opencv.hpp>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_hankyo_jeong_lachapelle_MainActivity_stringFromJNI(
@@ -7,4 +8,12 @@ Java_com_hankyo_jeong_lachapelle_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_hankyo_jeong_lachapelle_MainActivity_detectEdgeJNI(JNIEnv *env, jobject thiz,
+                                                            jlong input_image, jlong output_image,
+                                                            jint th1, jint th2) {
+    cv::Mat &inputMat = *(cv::Mat *)input_image;
 }
