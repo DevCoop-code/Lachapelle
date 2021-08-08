dc5bd0f12f7c63f8ae543d4fb36d3b95ddd4d028
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2021-08-08 16:29:05 +0900

    feat: Add Runtime Permission for access external Storage

--- app/src/main/java/com/hankyo/jeong/lachapelle/MainActivity.java
+++ app/src/main/java/com/hankyo/jeong/lachapelle/MainActivity.java
@@ -78,7 +78,8 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                     boolean externalStoragePermissionAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     if (!externalStoragePermissionAccepted)
                     {
-
+                        String needPermMsg = getResources().getString(R.string.permissionNeedMsg);
+                        showDialogForPermission(needPermMsg);
                     }
                 }
                 break;
@@ -88,24 +89,25 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     @TargetApi(Build.VERSION_CODES.M)
     private void showDialogForPermission(String msg)
     {
-        String alertTitle = getResources().getString(R.string.permissionAlertTitle);
-        String alertYes = getResources().getString(R.string.yesAlert);
-        String alertNo = getResources().getString(R.string.noAlert);
+        String alertTitleMsg = getResources().getString(R.string.permissionAlertTitle);
+        String alertYesMsg = getResources().getString(R.string.yesAlert);
+        String alertNoMsg = getResources().getString(R.string.noAlert);
+        String warningPermsMsg = getResources().getString(R.string.permissionWarningMsg);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle(alertTitle);
+        builder.setTitle(alertTitleMsg);
         builder.setMessage(msg);
         builder.setCancelable(false);
-        builder.setPositiveButton(alertYes, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(alertYesMsg, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
             }
         });
-        builder.setNegativeButton(alertNo, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(alertNoMsg, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-//                Toast.makeText(this, )
+                Toast.makeText(getApplicationContext(), warningPermsMsg, Toast.LENGTH_LONG).show();
                 finish();
             }
         });
